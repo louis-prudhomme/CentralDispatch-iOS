472dd6a56ef425b8bbe1b21edc4468a904772a8d
Louis Prud'homme <louis.prudhomme@theodo.com>
2025-12-29 21:54:00 +0100

    fix: missing mock

--- Wine/WineRepository/Sources/WineRepositoryMock.swift
+++ Wine/WineRepository/Sources/WineRepositoryMock.swift
@@ -1,21 +1,14 @@
 import Dependencies
 import Foundation
+import SharedCommonPersistence
 import WineEntity
 
 public extension WineRepository {
     static func mock(
-        fetchAll: @escaping @MainActor @Sendable () async throws -> [WineBottleEntity] = { [] },
-        fetch: @escaping @MainActor @Sendable (UUID) async throws -> WineBottleEntity? = { _ in nil },
-        upsert: @escaping @MainActor @Sendable (WineBottleEntity) async throws -> Void = { _ in },
-        delete: @escaping @MainActor @Sendable (UUID) async throws -> Void = { _ in }
+        base: BaseRepository<WineBottleEntity> = .mock(),
     ) -> WineRepository {
         WineRepository(
-            base: .mock(
-                fetchAll: fetchAll,
-                fetch: fetch,
-                upsert: upsert,
-                delete: delete
-            )
+            base: base
         )
     }
 }
